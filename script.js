d465375e81a656f46567edbff50a363a551035b8
Emma Jean Poteet <271843@carmax.com>
2026-05-12 14:33:18 -0400

    Refactor project to allow user to change # of cells

--- script.js
+++ script.js
@@ -1,30 +1,68 @@
 const container = document.querySelector('#container');
+const changeGridButton = document.querySelector('#gridSize');
+let gridCount = 16;
 let currentRow = 1;
 
-for (i = 1; i <= 16; i++) {
+function eraseRowsAndCells () {
+    const rows = document.querySelectorAll('.rowDiv');
+    const cells = document.querySelectorAll('.cell')
+
+    cells.forEach(cell => {
+        cell.remove();
+    })
+
+    rows.forEach(row => {
+        row.remove();
+    })
+}
+
+function generateRows(gridCount) {
+    for (i = 1; i <= gridCount; i++) {
     const newRow = document.createElement('div');
     newRow.id = `row${i}`;
     newRow.classList.add('rowDiv')
     container.appendChild(newRow);
+    }
 }
 
-const rows = document.querySelectorAll('.rowDiv');
+function generateCells(gridCount) {
+    const rows = document.querySelectorAll('.rowDiv');
 
-rows.forEach((row) => {
-    for (i = 1; i <= 16; i++) {
-        const newCell = document.createElement('div');
-        newCell.id = `cell${currentRow}-${i}`
-        newCell.classList.add('cell');
-        row.appendChild(newCell);
-    }
+    rows.forEach((row) => {
+        for (i = 1; i <= gridCount; i++) {
+            const newCell = document.createElement('div');
+            newCell.id = `cell${currentRow}-${i}`
+            newCell.classList.add('cell');
+            row.appendChild(newCell);
+        }
 
-    currentRow++
-})
-
-const cells = document.querySelectorAll('.cell')
-
-cells.forEach(cell => {
-    cell.addEventListener('mouseover', (e) => {
-        cell.style.backgroundColor = 'blue';
+        currentRow++
     })
+}
+
+function addCellEventListener() {
+    const cells = document.querySelectorAll('.cell')
+
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', (e) => {
+            cell.style.backgroundColor = 'blue';
+        })
+    })
+}
+
+changeGridButton.addEventListener('click', () => {
+    gridCount = parseInt(prompt('How many rows and columns would you like?', 16));
+    
+    if (gridCount !== NaN && gridCount < 100) {
+        eraseRowsAndCells();
+        generateRows(gridCount);
+        generateCells(gridCount);
+        addCellEventListener();
+    } else {
+        gridCount = parseInt(prompt('Please enter a valid number less than, or equal to, 100. How many rows and columns would you like?', 16));
+    }
 })
+
+generateRows(gridCount);
+generateCells(gridCount);
+addCellEventListener();
